400029e32a703639d9244edc1a0437e0c03e8d01
EC2 Default User <ec2-user@ip-172-31-50-42.ec2.internal>
2020-12-06 12:35:42 +0000

    [Fix]2-8

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -22,6 +22,12 @@ class TodolistsController < ApplicationController
     redirect_to todolist_path(list.id)
   end
   
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to todolists_path  # 投稿一覧画面へリダイレクト
+  end
+  
   def create
     # １. データを新規登録するためのインスタンス作成
     list = List.new(list_params)
